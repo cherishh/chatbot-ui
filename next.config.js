218a26b5d1ffbcf11b0df2f45dc35d5118eff881
wangzhongxi <wangzhongxi@meituan.com>
2023-04-26 18:36:06 +0800

    feat: next config add edge runtime

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   i18n,
   reactStrictMode: true,
-  experimental: { runtime: 'experimental-edge'},
+  experimental: { runtime: 'edge'},
 
   webpack(config, { isServer, dev }) {
     config.experiments = {
